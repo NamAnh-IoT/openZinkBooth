5eb6e927a723429be3f4f831be7cb093ce859c6c
oliexdev <olie.xdev@googlemail.com>
2026-05-08 15:29:44 +0200

    Bump version to 0.3 and update build dependencies to met f-droid gradle requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.photo.openzinkbooth"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
